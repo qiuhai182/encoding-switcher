66dc2e1be3971342c2cbbb804d09daad9c264477
邱海 <2810617217@qq.com>
2026-09-11 16:22:22 +0800

    快捷上传最新可执行文件、代码

--- upLoad.sh
+++ upLoad.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-git pull
-git pull
+git pull github
+git pull gitee
+python convert_to_utf8.py
 git add --all -- ':!nul'
 git commit -m "快捷上传最新可执行文件、代码"
-git push
+git push github
+git push gitee
